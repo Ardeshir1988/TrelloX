de3dff3c0105e98b67c0495840360961cce80ae8
ardeshir <ardeshir.ahouri@specific-group.com>
2022-09-13 16:52:40 +0430

    initial commit

---
+++ src/main/java/com/example/trellox/TrelloXApplication.java
@@ -0,0 +1,13 @@
+package com.example.trellox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trellox/TrelloXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trellox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
