9c916f3137ae7ab960d7ccf783d92abf0b0bb571
ardeshir <ardeshir.ahouri@specific-group.com>
2022-09-13 20:12:18 +0430

    impl register method

--- src/main/java/com/example/trellox/service/CustomerServiceImpl.java
+++ src/main/java/com/example/trellox/service/CustomerServiceImpl.java
@@ -2,16 +2,23 @@ package com.example.trellox.service;
 
 import com.example.trellox.dto.LoginDto;
 import com.example.trellox.dto.TokenDto;
+import com.example.trellox.enums.Role;
 import com.example.trellox.model.Customer;
 import com.example.trellox.repository.CustomerRepository;
+import com.example.trellox.security.JwtUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    private final JwtUtils jwtUtils;
     private final CustomerRepository customerRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(JwtUtils jwtUtils, CustomerRepository customerRepository) {
+        this.jwtUtils = jwtUtils;
         this.customerRepository = customerRepository;
     }
 
@@ -22,7 +29,18 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public TokenDto register(LoginDto loginDto) {
-        return null;
+        Optional<Customer> email = customerRepository.findByEmail(loginDto.getEmail());
+        if (email.isPresent())
+            throw new RuntimeException("This email is already registered");
+
+        Customer customer = customerRepository.save(Customer.builder()
+                .email(loginDto.getEmail())
+                .password(loginDto.getPassword())
+                .roles(List.of(Role.CUSTOMER))
+                .build());
+
+        String token = jwtUtils.generateToken(customer);
+        return TokenDto.builder().token(token).message("customer registered").build();
     }
 
     @Override
