eede41ba9111f2f99226323a0eb31ee65aa4b14c
ardeshir <ardeshir.ahouri@specific-group.com>
2022-09-13 18:23:12 +0430

    create CustomerServiceTest & write test for findCustomerByEmail method

---
+++ src/test/java/com/example/trellox/service/CustomerServiceTest.java
@@ -0,0 +1,47 @@
+package com.example.trellox.service;
+
+import com.example.trellox.enums.Role;
+import com.example.trellox.model.Customer;
+import com.example.trellox.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CustomerServiceImpl.class})
+public class CustomerServiceTest {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    private Customer getSampleCustomer() {
+        return Customer.builder()
+                .id("id-1")
+                .email("a.gmail.com")
+                .password("")
+                .roles(List.of(Role.CUSTOMER))
+                .build();
+    }
+
+    @Test
+    public void findCustomerByEmail_returnCustomer() {
+        when(customerRepository.findByEmail("a.gmail.com")).thenReturn(Optional.of(getSampleCustomer()));
+        Customer customer = customerService.findCustomerByEmail("a.gmail.com");
+        assertEquals(getSampleCustomer().getEmail(), customer.getEmail());
+        assertEquals(getSampleCustomer().getRoles().size(), 1);
+        assertEquals(getSampleCustomer().getRoles().stream().findAny().get(), Role.CUSTOMER);
+    }
+
+}
