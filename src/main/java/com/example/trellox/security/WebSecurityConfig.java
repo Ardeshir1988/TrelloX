d887616781b263843771fa74eaaa3e1ad673efd9
ardeshir <ardeshir.ahouri@specific-group.com>
2022-09-13 23:03:42 +0430

    impl TokenFilter & WebSecurityConfig

---
+++ src/main/java/com/example/trellox/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.trellox.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    @Bean
+    public TokenFilter authenticationJwtTokenFilter() {
+        return new TokenFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/signUp", "/api/login").permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
